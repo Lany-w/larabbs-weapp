bb8c92024692774f07488e45a2f0776e8aed3dd3
Lany <wlany@qq.com>
2019-11-07 14:37:13 +0800

    user pemrs

--- src/mixins/replyMixin.js
+++ src/mixins/replyMixin.js
@@ -13,6 +13,51 @@ export default class ReplyMixin extends wepy.mixin {
     // 当前页数
     page: 1
   }
+   methods = {
+    // 删除回复
+    async deleteReply(topicId, replyId) {
+      // 确认是否删除
+      let res = await wepy.showModal({
+        title: '确认删除',
+        content: '您确认删除该回复吗',
+        confirmText: '删除',
+        cancelText: '取消'
+      })
+
+      // 点击取消后返回
+      if (!res.confirm) {
+        return
+      }
+      try {
+        // 调用接口删除回复
+        let deleteResponse = await api.authRequest({
+          url: 'topics/' + topicId + '/replies/' + replyId,
+          method: 'DELETE'
+        })
+
+        // 删除成功
+        if (deleteResponse.statusCode === 204) {
+          wepy.showToast({
+            title: '删除成功',
+            icon: 'success',
+            duration: 2000
+          })
+          // 将删除了的回复移除
+          this.replies = this.replies.filter((reply) => reply.id !== replyId)
+          this.$apply()
+        }
+
+        return deleteResponse
+      } catch (err) {
+        console.log(err)
+        wepy.showModal({
+          title: '提示',
+          content: '服务器错误，请联系管理员'
+        })
+      }
+    }
+  }
+
   // 获取话题回复
   async getReplies(reset = false) {
     try {
@@ -29,7 +74,13 @@ export default class ReplyMixin extends wepy.mixin {
         let replies = repliesResponse.data.data
 
         // 格式化回复创建时间
+        let user = await this.$parent.getCurrentUser()
         replies.forEach(function (reply) {
+           if (!user) {
+                reply.can_delete = false
+            } else {
+                reply.can_delete = (reply.user_id === user.id || this.$parent.can('manage_contents'))
+            }
           reply.created_at_diff = util.diffForHumans(reply.created_at)
         })
         // 如果reset不为true则合并 this.replies；否则直接覆盖
@@ -53,6 +104,7 @@ export default class ReplyMixin extends wepy.mixin {
       })
     }
   }
+  
   async onPullDownRefresh() {
     this.noMoreData = false
     this.page = 1
